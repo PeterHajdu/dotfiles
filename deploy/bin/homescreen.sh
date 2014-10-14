95d3014aa3e62588bc9272b3e2b46af15c405fe2
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-14 08:42:21 +0200

    fix homescreen script

--- deploy/bin/homescreen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-xrandr --newmode  "1440x900"  106.50  1440 1528 1672 1904  900 903 909 934 -hsync +vsync
-xrandr --addmode VGA1 1440x900
-xrandr --output VGA1 --mode 1440x900
-xrandr --output LVDS1 --mode 1024x600
-xrandr --output VGA1 --auto --right-of LVDS1
-
